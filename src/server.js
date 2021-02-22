f70b38f3ef338ae72f2f5502c87bd838acdfc165
Priya Gupta <pgupta000205@gmail.com>
2021-02-22 21:17:36 +0530

    chore: boilerplate and health check

---
+++ src/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const env = require('dotenv');
+const { DEFAULT_PORT } = require('./constants/config');
+const { healthRouter } = require('./routes');
+
+const app = express();
+env.config();
+const port = process.env.PORT || DEFAULT_PORT;
+
+app.use('/ping', healthRouter);
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
