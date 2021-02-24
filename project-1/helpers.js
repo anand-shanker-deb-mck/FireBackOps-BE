46a239f6497cbeb9a85ccbbb8a6b5f93f6ea8809
swetha <swetha.gumpena0607@gmail.com>
2021-02-24 19:19:36 +0530

    fix: Deleted  generated files

--- project-1/helpers.js
+++
@@ -1,9 +0,0 @@
-const fetch = require('node-fetch');
-
-const makeAPIcall = async (endpoint, method = 'GET') => {
-  const response = await fetch(endpoint).then((res) => res.json());
-  return response;
-};
-module.exports = {
-  makeAPIcall,
-};
--- project-1/index.js
+++
@@ -1,8 +0,0 @@
-const { makeAPIcall } = require('./helpers');
-
-async function getCheapestPrice() {
-  const c1 = await makeAPIcall('http://demo1852771.mockable.io/ibibo');
-  const c2 = await makeAPIcall('http://demo1852771.mockable.io/mmt');
-  const c3 = c1.price < c2.price ? c1.price : c2.price;
-  return c3;
-}
