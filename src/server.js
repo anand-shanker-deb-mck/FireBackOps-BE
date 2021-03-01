98abbb63b09f9426aa685a94fda6181ed407401f
isha2912 <ishadeep222@gmail.com>
2021-03-01 17:21:53 +0530

    feat(models): add models and migration

--- src/server.js
+++ src/server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const env = require('dotenv');
+const morgan = require('morgan');
 const { DEFAULT_PORT } = require('./constants/config');
 const { healthRouter } = require('./routes');
+const winston = require('./config/winston');
 
 const app = express();
 env.config();
 const port = process.env.PORT || DEFAULT_PORT;
 
+app.use(express.json());
+app.use(morgan('combined', { stream: winston.stream }));
 app.use('/ping', healthRouter);
 
 app.listen(port, () => {
