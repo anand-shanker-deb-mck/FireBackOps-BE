982d581efbac99febfc9c8a27e006ffc2743b927
Apoorva <apoorva.choudhary@iiitb.org>
2021-03-02 18:20:25 +0530

    feat(service): add oauth login service - get access token and user details , generate signed JWTtoken  and returns it

---
+++ src/service/login.service.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+const fileUtil = require('../utils/file.util');
+const githubApiUtil = require('../utils/github.api.util');
+
+const oauthLogin = async (code) => {
+  // eslint-disable-next-line no-useless-catch
+  try {
+    const body = {
+      client_id: process.env.CLIENT_ID,
+      client_secret: process.env.CLIENT_SECRET,
+      code,
+    };
+    const opts = { headers: { accept: 'application/json' } };
+    const accessToken = await githubApiUtil.getToken(body, opts);
+    const username = await githubApiUtil.getUserName(accessToken);
+    fileUtil.writeFile('accessToken.txt', `${username} ${accessToken}`);
+    const jwtToken = jwt.sign({ username },
+      process.env.JWT_SECRET);
+    return jwtToken;
+  } catch (error) {
+    throw error;
+  }
+};
+
+module.exports = { oauthLogin };
