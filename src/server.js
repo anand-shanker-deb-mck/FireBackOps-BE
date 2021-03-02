2ab51294c5fcbc793cc3d1aec95ec8311aa9d41f
Apoorva <apoorva.choudhary@iiitb.org>
2021-03-02 18:14:32 +0530

    feat(route): add middleware to access static files with express

--- src/server.js
+++ src/server.js
@@ -2,16 +2,18 @@ const express = require('express');
 const env = require('dotenv');
 const morgan = require('morgan');
 const { DEFAULT_PORT } = require('./constants/config');
-const { healthRouter } = require('./routes');
+const { healthRouter, loginRouter } = require('./routes');
 const winston = require('./config/winston');
 
 const app = express();
 env.config();
 const port = process.env.PORT || DEFAULT_PORT;
-
+app.use(express.static(`${__dirname}/template`));
 app.use(express.json());
 app.use(morgan('combined', { stream: winston.stream }));
+
 app.use('/ping', healthRouter);
+app.use('/login', loginRouter);
 
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
