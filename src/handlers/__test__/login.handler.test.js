2951f5b41a26da7b41507322553951e42697bffd
Apoorva <apoorva.choudhary@iiitb.org>
2021-03-02 20:07:05 +0530

    feat(handler): add try catch inside handlers and add test cases which throws error

--- src/handlers/__test__/login.handler.test.js
+++ src/handlers/__test__/login.handler.test.js
@@ -1,13 +1,18 @@
 const { loginHandler, oauthLoginHandler } = require('../login.handler');
 const service = require('../../service/login.service');
 
+// ---------------- loginHandler-------------------------------------------//
 describe('loginHandler', () => {
   let mockResponse;
+  let mockSend;
   beforeEach(() => {
+    mockSend = jest.fn();
     mockResponse = {
+      status: jest.fn(() => ({ send: mockSend })),
       redirect: jest.fn(),
     };
   });
+
   it('should go to if block where body token is undefined', () => {
     const mockRequest = {
       body: {},
@@ -15,8 +20,17 @@ describe('loginHandler', () => {
     loginHandler(mockRequest, mockResponse);
     expect(mockResponse.redirect).toHaveBeenCalledWith(`/login.html?client_id=${process.env.CLIENT_ID}`);
   });
+
+  it('should go to catch block when there is no body', () => {
+    const mockRequest = {
+    };
+    loginHandler(mockRequest, mockResponse);
+    expect(mockResponse.status).toHaveBeenCalledWith(500);
+    expect(mockSend).toHaveBeenCalledWith();
+  });
 });
 
+// ---------------- oauthLoginHandler------------------------------------------//
 describe('oauthLoginHandler', () => {
   let mockSend;
   let mockResponse;
@@ -26,6 +40,7 @@ describe('oauthLoginHandler', () => {
       status: jest.fn(() => ({ send: mockSend })),
     };
   });
+
   it('should return with status 200 and jwt token', async () => {
     const mockRequest = {
       query: { code: '124512' },
@@ -35,5 +50,15 @@ describe('oauthLoginHandler', () => {
     await oauthLoginHandler(mockRequest, mockResponse);
     expect(mockResponse.status).toHaveBeenCalledWith(200);
     expect(mockSend).toHaveBeenCalledWith({ JWTtoken: token });
+  });
+
+  it('should go to catch block when service throws error ', async () => {
+    const mockRequest = {
+      query: { code: '124512' },
+    };
+    jest.spyOn(service, 'oauthLogin').mockImplementation(() => { throw new Error('error'); });
+    await oauthLoginHandler(mockRequest, mockResponse);
+    expect(mockResponse.status).toHaveBeenCalledWith(500);
+    expect(mockSend).toHaveBeenCalledWith();
   });
 });
--- src/handlers/login.handler.js
+++ src/handlers/login.handler.js
@@ -1,18 +1,25 @@
 const service = require('../service/login.service');
 
 const loginHandler = (req, res) => {
-  const { body } = req;
-  if (body.token === undefined) {
-    res.redirect(`/login.html?client_id=${process.env.CLIENT_ID}`);
-    return;
+  try {
+    const { body } = req;
+    if (body.token === undefined) {
+      res.redirect(`/login.html?client_id=${process.env.CLIENT_ID}`);
+      return;
+    }
+  } catch (error) {
+    res.status(500).send();
   }
-  res.status(200).send();
 };
 
 const oauthLoginHandler = async (req, res) => {
-  const { query } = req;
-  const token = await service.oauthLogin(query.code);
-  res.status(200).send({ JWTtoken: token });
+  try {
+    const { query } = req;
+    const token = await service.oauthLogin(query.code);
+    res.status(200).send({ JWTtoken: token });
+  } catch (error) {
+    res.status(500).send();
+  }
 };
 
 module.exports = {
