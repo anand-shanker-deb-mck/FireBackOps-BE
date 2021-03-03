1022935f1eebdbab08ab33e114ba819a987a7893
asmitahajra <30727326+asmitahajra@users.noreply.github.com>
2021-03-03 18:28:07 +0530

    Feat/swagger (#49)
    * feat(swagger): update server.js to import swagger-ui
    
    * feat(swagger): update package.json to install swagger-ui-express
    
    * feat(swagger): add swagger.json specification for health route
    
    * feat(swagger): update swagger.json specification for health route

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const env = require('dotenv');
 const morgan = require('morgan');
+const swaggerUi = require('swagger-ui-express');
 const { DEFAULT_PORT } = require('./constants/config');
 const { healthRouter } = require('./routes');
 const winston = require('./config/winston');
+const swaggerDocument = require('./swagger.json');
 
 const app = express();
 env.config();
@@ -12,6 +14,7 @@ const port = process.env.PORT || DEFAULT_PORT;
 app.use(express.json());
 app.use(morgan('combined', { stream: winston.stream }));
 app.use('/ping', healthRouter);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
