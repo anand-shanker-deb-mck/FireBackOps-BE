a0030f720f7cf961f6dc0881c580286d2fc98921
swetha <swetha.gumpena0607@gmail.com>
2021-03-03 21:35:55 +0530

    feat: Add validation for API component

--- src/middleware/validation/schema.js
+++ src/middleware/validation/schema.js
@@ -1,3 +1,4 @@
+const isUrl = require('is-url');
 const Joi = require('joi');
 
 const APICOMPONENT = 'API';
@@ -10,6 +11,18 @@ const storeConfigDBSchema = Joi.object().keys({
 
 const apiSchema = Joi.object().keys({
   method: Joi.string().valid('GET', 'POST', 'PUT', 'DELETE').required(),
+  url: Joi.string().custom((urlValue, helper) => {
+    if (!isUrl(urlValue)) {
+      return helper.message('URL must be valid');
+    }
+    return true;
+  }),
+  headers: Joi.object(),
+  // body is valid only for POST and PUT methods
+  body: Joi.object().when('method', { is: 'POST', then: Joi.required() })
+    .concat(Joi.object().when('method', { is: 'PUT', then: Joi.required() }))
+    .concat(Joi.object().when('method', { is: 'GET', then: Joi.forbidden() }))
+    .concat(Joi.object().when('method', { is: 'DELETE', then: Joi.forbidden() })),
 });
 
 module.exports = { storeConfigDBSchema, apiSchema };
--- src/middleware/validation/validator.js
+++ src/middleware/validation/validator.js
@@ -1,18 +1,12 @@
-/* eslint-disable no-unused-vars */
 const { storeConfigDBSchema, apiSchema } = require('./schema');
-const { payloadValidation } = require('./customValidation');
 
 const storeConfigValidator = (req, res, next) => {
   const { body } = req;
-  const { value, error } = storeConfigDBSchema.validate(body);
-  //   const msg = payloadValidation(body);
+  const { error } = storeConfigDBSchema.validate(body);
 
   if (error) {
-    res.status(400).send();
+    res.status(400).json({ error: 'Invalid component' });
   }
-  //   } else {
-  //     res.status(200).send({ message: ' Valid data' });
-  //   }
   next();
 };
 
@@ -20,15 +14,12 @@ const payloadValidator = (req, res) => {
   const { body } = req;
   const { payload } = body;
   if (body.type === 'API') {
-    console.log(payload);
-    const { value, error } = apiSchema.validate(payload);
+    const { error } = apiSchema.validate(payload);
 
     if (error) {
-      console.log('error');
-      res.status(400).send();
+      res.status(400).json({ error: error.message });
     } else {
-      console.log('success');
-      res.status(200).send({ message: ' Valid payload' });
+      res.status(200).json({ message: 'Valid payload' });
     }
     // next();
   }
