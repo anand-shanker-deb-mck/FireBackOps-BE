2ae4e874729f3526a7490d7f9fd6851f5d289847
isha2912 <ishadeep222@gmail.com>
2021-03-04 17:58:46 +0530

    feat: username, password and db details from env file

---
+++ config/config.js
@@ -0,0 +1,27 @@
+const env = require('dotenv');
+
+env.config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOSTNAME,
+    dialect: 'postgres',
+  },
+  test: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOSTNAME,
+    dialect: 'postgres',
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOSTNAME,
+    dialect: 'postgres',
+  },
+};
