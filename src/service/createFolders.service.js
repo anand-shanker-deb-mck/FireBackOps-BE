fa48407b9c5abd5d33b5cf2ced9c07eef3ad20e7
isha2912 <ishadeep222@gmail.com>
2021-03-15 08:46:27 +0530

    fix: async logic

--- src/service/createFolders.service.js
+++ src/service/createFolders.service.js
@@ -2,7 +2,6 @@
 const {
   Project, Route, Configuration,
 } = require('../../models');
-
 const createFoldersHelper = require('./createFolder.service.helper');
 
 const getRouteDetailsService = async (projectId) => {
@@ -11,26 +10,20 @@ const getRouteDetailsService = async (projectId) => {
       model: Route,
       as: 'routes',
       attributes: [['name', 'routeName']],
-
       include: [{
         model: Configuration,
         as: 'configurations',
         attributes: ['componentType', 'payload', 'sequence', 'refName', 'dependencies'],
-
       }],
     }],
     where: {
       id: projectId,
     },
-
     attributes: [['name', 'projectName']],
   });
 
   const newProjectDetails = JSON.parse(JSON.stringify(projectDetails));
-  // console.log(newProject.routes[0].configurations);
-
   const filteredDetails = await createFoldersHelper.filterDetails(newProjectDetails);
-
   return filteredDetails;
 };
 
