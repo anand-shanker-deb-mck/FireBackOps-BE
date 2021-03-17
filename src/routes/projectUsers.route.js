6c9d252c00f0745229720a3c31f140a68b6b0777
isha2912 <ishadeep222@gmail.com>
2021-03-17 23:07:50 +0530

    add authorization

--- src/routes/projectUsers.route.js
+++ src/routes/projectUsers.route.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const { authenticateJwt } = require('../middlewares/auth.middleware');
 const projectUsersHandler = require('../handlers/projectUsers.handler');
 const { projectUsersValidator } = require('../middlewares/projectUsers.validator');
 
 const router = express.Router();
 
-router.get('/:id', projectUsersValidator, projectUsersHandler.getUserNames);
+router.get('/:id', projectUsersValidator, authenticateJwt, projectUsersHandler.getUserNames);
 
 module.exports = { router };
