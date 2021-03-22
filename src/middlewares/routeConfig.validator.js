12f97a6e2d883cfe0b1da773fe613fb64efb72ce
isha2912 <ishadeep222@gmail.com>
2021-03-22 14:12:21 +0530

    fix: eslint issues

--- src/middlewares/routeConfig.validator.js
+++ src/middlewares/routeConfig.validator.js
@@ -1,13 +1,12 @@
 const { routeConfigSchema } = require('./routeConfig.validation.schema');
 
-// eslint-disable-next-line consistent-return
 const routeConfigValidator = (req, res, next) => {
   const { params } = req;
   const validate = routeConfigSchema.validate(params);
   if (validate.error) {
     return res.status(400).send('Bad Request');
   }
-  next();
+  return next();
 };
 
 module.exports = {
