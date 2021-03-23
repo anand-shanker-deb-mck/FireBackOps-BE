92a57081e5b57382e43b45f698b34621ca8030e8
Priya <49350993+priyagupta14@users.noreply.github.com>
2021-03-23 19:19:53 +0530

    feat: add cors as middleware (#129)

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const env = require('dotenv');
 const morgan = require('morgan');
-
+const cors = require('cors');
 const { DEFAULT_PORT } = require('./constants/config');
 const { router } = require('./routes');
 const winston = require('../config/winston');
@@ -10,6 +10,7 @@ env.config();
 const port = process.env.PORT || DEFAULT_PORT;
 const app = express();
 
+app.use(cors());
 app.use(express.static(`${__dirname}/template`));
 app.use(express.json());
 app.use(morgan('combined', { stream: winston.stream }));
