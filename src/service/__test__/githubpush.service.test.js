bb5cac7c64082c31b788ca185845f740a63d2afa
Varsha C L <varsha_c_l@mckinsey.com>
2021-04-05 12:47:43 +0530

    fix: octokit mock

--- src/service/__test__/githubpush.service.test.js
+++ src/service/__test__/githubpush.service.test.js
@@ -1,10 +1,15 @@
 /* eslint-disable no-unused-vars */
 jest.mock('redis', () => jest.requireActual('redis-mock'));
-jest.mock('@octokit/core');
-const { Octokit } = require('@octokit/core');
 const githubPushUtils = require('../../utils/pushToGithub');
 const redisUtil = require('../../utils/redis.util');
 const githubPushServices = require('../githubPush.service');
+
+const mockResponse = { data: { html_url: 'url to pull' } };
+jest.mock('@octokit/core', () => ({
+  Octokit: jest.fn().mockImplementation(() => (
+    { request: jest.fn().mockReturnValue(mockResponse) }
+  )),
+}));
 
 describe('githubpush service', () => {
   const body = {
@@ -29,13 +34,10 @@ describe('github raise pr', () => {
     branchName: 'feature-branch',
   };
   const token = 'dummy_token';
-  const octokit = { request: jest.fn() };
-  const mockResponse = { data: { html_url: 'url to pull' } };
+
   test('Raise pull request on call', async () => {
-    // jest.spyOn(redisUtil, 'getAccessToken').mockResolvedValue(token);
-    // const octokit = new Octokit({ auth: token });
-    // jest.spyOn(octokit, 'request').mockResolvedValue(mockResponse);
-    // const pullReqUrl = await githubPushServices.githubRaisePullRequest(body, 'abc');
-    // expect(pullReqUrl).toBe('url to pull');
+    jest.spyOn(redisUtil, 'getAccessToken').mockResolvedValue(token);
+    const pullReqUrl = await githubPushServices.githubRaisePullRequest(body, 'abc');
+    expect(pullReqUrl).toBe('url to pull');
   });
 });
